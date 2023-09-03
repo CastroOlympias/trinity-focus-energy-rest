c54f04918d323842f2fcd824a5612ed429464a93
CastroOlympias <blackcarrera@msn.com>
2023-09-02 18:07:35 -0600

    cleaned up the user controller login api and right before user data is returned to client changed the userdata password to empty string so the hashed password is sent back to client, can't do a select -password as the password needs to return from the database to authenticate

--- data_connections/userConnections/UserController.js
+++ data_connections/userConnections/UserController.js
@@ -1,14 +1,14 @@
 const { UserModel } = require('../../data_models/_index_models');
 const { signToken } = require('../../utils/Authentication')
 const bcrypt = require('bcrypt');
+
 const userController = {
-  // get all pizzas
   getAllUsers: async (req, res) => {
-    // console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$', req)
     UserModel.find({})
       // .limit(1)
       // .skip(0)
       .select('-__v')
+      .select('-password')
       .sort({ _id: -1 })
       .populate('userHome')
       .then(userData => res.json(userData))
@@ -26,36 +26,23 @@ const userController = {
     return { token, newUser }
   },
 
-
-
-
   loginToUser: async ({ body }, res) => {
-
     const compareIncomingPasswordWithDatabasePassword = async (userData) => {
-      console.log(userData)
 
       if (userData === null) {
         return MESSAGE = { WRONG_CREDENTIALS: 'You have provided the wrong credentials' }
       }
-
       const checkLoginCredentials = await bcrypt.compare(body.password, userData.password);
-
-
-      console.log('Password match?', checkLoginCredentials)
-
       if (!checkLoginCredentials) {
-        console.log('Invalid credentials')
         return MESSAGE = { WRONG_CREDENTIALS: 'You have provided the wrong credentials' }
       } else {
-        console.log('correct password')
+        userData.password = ''
         const token = signToken(userData);
         return { token, userData };
       }
 
     }
-
-    const user = await UserModel.findOne({ eMail: body.eMail })
-
+    await UserModel.findOne({ eMail: body.eMail })
       .sort({ _id: -1 })
       .populate('userHome')
       .then(userData => compareIncomingPasswordWithDatabasePassword(userData))
@@ -64,45 +51,7 @@ const userController = {
         console.log(err);
         res.sendStatus(400);
       });
-    // console.log(user)
-
-
   }
-
-
-
-  // loginToUser: async ({ body }, res) => {
-
-  //   const user = await UserModel.findOne({ eMail: body.eMail })
-
-  //     .sort({ _id: -1 })
-  //     .populate('userHome')
-  //     .then(userData => res.json(userData))
-  //     .catch(err => {
-  //       console.log(err);
-  //       res.sendStatus(400);
-  //     });
-  //   if (!user) {
-  //     console.log('Invalid credentials')
-  //   }
-  //   let salt = 10
-  //   const test = await bcrypt.compare(body.password, await bcrypt.hash(body.password, salt));
-
-
-  //   console.log('Password match?', test)
-  //   if (!test) {
-  //     console.log('Invalid credentials')
-  //   } else {
-  //     console.log('correct password')
-  //   }
-
-  //   const token = signToken(user);
-  //   return { token, user };
-  // }
-
-
-
-
 };
 
 module.exports = userController;
