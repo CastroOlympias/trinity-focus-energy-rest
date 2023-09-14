e9e1f25d5c4e8dd3f02d9333d0cf3b9eba2b0775
CastroOlympias <blackcarrera@msn.com>
2023-09-13 18:29:43 -0600

    conditional rendering does work using a global state variable, have a method to refresh DOM elements but as it is now it refreshes everything in the body and all children, by recalling body.innerHtml = TrinityFocusEnergy() and because the global variable state change from true and false the refreshed DOM renders the new data back and forth as I press the button to change the text from true and false, need to figoure how to refresh individual children wihout update the whole body

--- public/trinity focus energy/TrinityFocusEnergy.mjs
+++ public/trinity focus energy/TrinityFocusEnergy.mjs
@@ -4,16 +4,30 @@ import ActionBar from "./platform components/actionBar/ActionBar.mjs"
 
 const testFunction = () => {
   console.log("Hello John", isTrue)
+  const first = document.getElementById('isTrue')
+  first.textContent = "true"
+  const second = document.getElementById('isFalse')
+
+  const third = document.getElementById('trinigy-focus-energy-body-section')
+  third.className = 'body-section BackDrop-mediumGrey-theme plusAnother'
   if (isTrue === false) {
     isTrue = true
+  } else {
+    isTrue = false
   }
-  // if (isTrue === true) {
-  //   isTrue = false
-  // }
+
+  if (isFalse === true) {
+    isFalse = false
+  } else {
+    isFalse = true
+  }
+
   console.log("Hello John", isTrue)
-  registeredFunctions.renderFunction()
+
   // const elemnetIsTrue = document.getElementById('isTrue')
   // elemnetIsTrue.textContent = isTrue
+  const body = document.getElementById('body')
+  registeredFunctions.retriggerRender(body)
 }
 
 // console.log(DOM[0].children[1].children)
@@ -22,50 +36,49 @@ export default function TrinityFocusEnergy(hello_world) {
   const fromHtml = './trinity focus energy/'
   hello_world = "Hello World from TinityFocusEnergy"
 
-  const renderFunction = () => {
-    setTimeout(() => {
-      if (isTrue === false) {
-        isTrue = true
-      }
-      console.log('jdjdjdjdjdjdjdjdjdjd')
-    }, 2000)
 
+  // setTimeout(() => {
+  //   if (isTrue === false) {
+  //     isTrue = true
+  //   }
+  //   console.log('jdjdjdjdjdjdjdjdjdjd')
+  // }, 2000)
 
-    console.log(isTrue)
-    return (`
+  registeredFunctions = { ...registeredFunctions, testFunction: testFunction, Home: Home }
+  console.log(isTrue)
+  return (`
       <section id='trinigy-focus-energy-body-section' class='body-section BackDrop-mediumGrey-theme'>
       <h1 id='isTrue'>${isTrue}</h1>
+      <h1 id='isFalse'>${isFalse}</h1>
         <section id='trinity-focus-energy-section' class='trinity-focus-energy-section'>
         
           ${isTrue === true ? (
-        `<h1>Hello!!!!</h1>`
-      ) : (
-        `<h1>No!!!!</h1>`
-      )}
+      `<h1>Hello!!!!</h1>`
+    ) : (
+      `<h1>No!!!!</h1>`
+    )}
 
           ${Home()}
         </section >
         ${ActionBar()}
       </section >
     `)
-  }
-
-  registeredFunctions = { ...registeredFunctions, testFunction: testFunction, renderFunction: renderFunction }
-  return renderFunction()
-
-
-  //   return (`
-  //   <section id='trinigy-focus-energy-body-section' class='body-section BackDrop-mediumGrey-theme'>
-  //     <section id='trinity-focus-energy-section' class='trinity-focus-energy-section'>
-  //     ${isTrue === true ? (
-  //     `<h1>Hello!!!!</h1>`
-  //   ) : (
-  //     `<h1>No!!!!</h1>`
-  //   )}
-
-  //       ${Home()}
-  //     </section >
-  // ${ActionBar()}
-  //   </section >
-  // `)
 }
+
+
+
+
+//   return (`
+//   <section id='trinigy-focus-energy-body-section' class='body-section BackDrop-mediumGrey-theme'>
+//     <section id='trinity-focus-energy-section' class='trinity-focus-energy-section'>
+//     ${isTrue === true ? (
+//     `<h1>Hello!!!!</h1>`
+//   ) : (
+//     `<h1>No!!!!</h1>`
+//   )}
+
+//       ${Home()}
+//     </section >
+// ${ActionBar()}
+//   </section >
+// `)
--- public/trinity focus energy/platform components/actionBar/ActionBar.mjs
+++ public/trinity focus energy/platform components/actionBar/ActionBar.mjs
@@ -200,6 +200,7 @@ const closeAllModals = () => {
 }
 
 const navigateHome = (test) => {
+
   console.log('Navigate home function at Actionbar Module', test)
   // console.log(currentUrlRoute)
   const topDashBoardSection = document.getElementById(`TopDashboard-Section`)
